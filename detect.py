065d61bc5e6b074adcfc4f609550bea68f7a32d7
Rishabh K <65342857+Rishabh3112002@users.noreply.github.com>
2022-02-03 23:06:28 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-#import main
+import main
 from imutils.video import VideoStream
 import numpy as np
 import imutils
